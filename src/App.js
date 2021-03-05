70de50816ebbdb8dd460fda0d10900db4df8fb66
bKedds <benkeddie@gmail.com>
2021-03-05 11:40:13 -0800

    added header component and shop page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from './components/header/header.component';
 
 function App() {
 	return (
 		<div>
-			<Switch>
-				<Route exact path='/' component={HomePage} />
-			</Switch>
+			<BrowserRouter>
+      <Header />
+				<Switch>
+					<Route exact path='/' component={HomePage} />
+					<Route path='/shop' component={ShopPage} />
+				</Switch>
+			</BrowserRouter>
 		</div>
 	);
 }
